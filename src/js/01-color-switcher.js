7171b49372b18e93db0169c29af6470527bc52d3
Margarita Dieltsova <margolede@gmail.com>
2023-02-26 16:59:18 +0200

    upDated

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,32 +6,28 @@
 // // Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. Зроби так,
 //  щоб доки зміна теми запущена, кнопка «Start» була неактивною(disabled).
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  // setTimeout(() => {
-  //   console.log('change color');
-  // }, delay);
-}
-
-const refs = {
-  start: document.querySelector('button[data-start]'),
-  stop: document.querySelector('button[data-stop]'),
-};
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
 colorInterval = null;
-refs.start.disabled = false;
+startBtn.disabled = false;
 
-refs.start.addEventListener('click', changeColor);
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', ClearChangeColor);
+
 function changeColor() {
-  refs.start.disabled = true;
-  refs.stop.disabled = false;
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
   colorInterval = setInterval(() => {
     document.body.style.background = getRandomHexColor();
   }, 1000);
 }
-refs.stop.addEventListener('click', ClearChangeColor);
+
 function ClearChangeColor() {
-  refs.stop.disabled = true;
-  refs.start.disabled = false;
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
   clearInterval(colorInterval);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
