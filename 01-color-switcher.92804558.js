aad9842584d33eb054b3448d4b2f259fd20388fc
MargaritaDel <112723193+MargaritaDel@users.noreply.github.com>
2023-02-26 15:04:00 +0000

    Deploying to gh-pages from @ MargaritaDel/goit-js-hw-09@9273242306cdf3ac86809e33347d85fa698f424b 🚀

--- 01-color-switcher.92804558.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");colorInterval=null,t.disabled=!1,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,colorInterval=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,clearInterval(colorInterval)}));
-//# sourceMappingURL=01-color-switcher.92804558.js.map
